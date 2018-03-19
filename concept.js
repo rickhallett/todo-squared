398fe7bc0397c1d7dd3bdfdf72436bcb0e1dcead
whitefire0 <rickhallett@live.co.uk>
2018-03-19 16:25:10 +0000

    draft data structure

--- concept.js
+++ concept.js
@@ -1,94 +1,161 @@
 console.log('concept.js loaded');
 
-//have root list (base parent)
-//have a way of adding todos to this list.
-//each todo can itself be a parent
-//do we need a todo id?
-  //no, as we can create a DOM identifier from the lists array position and list name (so its unique)
-
-//needs way of dynamically creating sublist names based on parent
-
-// =================================
-//      mixed data types
-// =================================
 /*
-var todoList = {
-  name: '$root',
-  rootTodos: []
-}
-
-function createTodo(name, parent) {
-  var list = {};
-  list.name = name;
-  list.parent = parent || null;
-  list.subTodos = [];
-  return list;
-}
-
-todoList.rootTodos.push(createTodo('todo 1'));
-todoList.rootTodos.push(createTodo('todo 2'));
-todoList.rootTodos.push(createTodo('todo 3'));
-
-todoList.rootTodos[0].subTodos.push(createTodo('todo 1-1'));
-todoList.rootTodos[0].subTodos[0].subTodos.push(createTodo('todo 2-1'));
-todoList.rootTodos[0].subTodos[0].subTodos[0].subTodos.push(createTodo('todo 3-1'));
-
-console.log(todoList);
-
-
-// =================================
-//    nested objects
-// =================================
-//What is the problem? How can we start at the root list, and have a function traverse 'down' its children if it doesn't keep a log of its children? As it stands, traversing upwards is fine as parents are stored
-//How can we add a 'child' property that has a consistently repeating structure?
-
-function List(name, parent) {
-  this.parent = parent || null;
-  this.name = name;
-}
-
-var list = new List('root-list');
-var childList = new List('child-list', list);
-var siblingList = new List('sibling-list', list);
-var secondGenChildList = new List('second-gen', childList);
-
-console.log(list)
-console.log(childList)
-console.log(siblingList)
-console.log(secondGenChildList)
+Todo List:
+1. create recursive functions that 'walk' rootTodo, printing out each layer
 */
 
+
+//create a function that takes the parent text and child text
+//create the function on the data object, so that it can traverse with 'this'
+//if no parent text, make parent text 'root'
+
+//the data structure is a series of nested objects
+//each object needs a unique identifier, todo text, completed property, and a reference to both its parent and any child todos
+
 // =================================
-//     function constructors
+//          prototype
 // =================================
 
-//make a function that creates an object
-function Todo(name, parent) {
-  this.name = name;
-  this.parent = parent || null;
-  this.siblings = [];
+
+
+
+$todoListDemo = {
+  $root: [
+    {
+      parent: '$root',
+      text: 'Complete watchandcode',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: null
+    },
+    {
+      parent: '$root',
+      text: 'master js',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: [
+        {
+          parent: 'master js',
+          text: 'master vue.js',
+          completed: false,
+          dateCreated: '19/04/2018',
+          subTodo: null
+        }
+      ]
+    },
+  ]
 }
 
-//have a method on that object that adds a child property which itself is a copy of the original object
-Todo.prototype.createChild = function(name, parent) {
-  this.child = new Todo(name, parent);
-}
-
-Todo.prototype.createSibling = function(name) {
-  var todo = new Todo(name, this);
-  this.siblings.push(todo);
+$todoList = {
+  $root: [
+    {
+      parent: '$root',
+      text: 'Complete watchandcode',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: null
+    },
+    {
+      parent: '$root',
+      text: 'become a javascript ninja',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: [
+        {
+          parent: 'become a javascript ninja',
+          text: 'complete BYOA',
+          completed: false,
+          dateCreated: '19/04/2018',
+          subTodo: [
+            {
+              parent: 'complete BYOA',
+              text: 'complete scope inheritance',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            },
+            {
+              parent: 'complete BYOA',
+              text: 'move onto data parsing',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            }
+          ]
+        },
+        {
+          parent: 'become a javascript ninja',
+          text: 'master vue.js',
+          completed: false,
+          dateCreated: '19/04/2018',
+          subTodo: [
+            {
+              parent: 'master vue.js',
+              text: 'complete tutorial and documentation',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            },
+            {
+              parent: 'master vue.js',
+              text: 'created nested todo app with vue',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            }
+          ]
+        },
+        {
+          text: 'beat Jonathan at javascript',
+          completed: false,
+          dateCreated: '19/04/2018',
+          subTodo: [
+            {
+              parent: 'beat Jonathan at javascript',
+              text: 'kick',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            },
+            {
+              parent: 'beat Jonathan at javascript',
+              text: 'his',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            },
+            {
+              parent: 'beat Jonathan at javascript',
+              text: 'ass',
+              completed: false,
+              dateCreated: '19/04/2018',
+              subTodo: null
+            }
+          ]
+        }
+      ]
+    },
+    {
+      parent: '$root', text: 'take out dog',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: [
+        {
+          text: 'pick up her poo',
+          completed: false,
+          dateCreated: '19/04/2018',
+          subTodo: null
+        }
+      ]
+    },
+    {
+      parent: '$root', text: 'find another hobby',
+      completed: false,
+      dateCreated: '19/04/2018',
+      subTodo: null
+    }
+  ]
 };
 
-var rootTodo = new Todo('Top layer')
-
-rootTodo.createChild('2nd layer', rootTodo)
-rootTodo.child.createChild('3rd layer', rootTodo.child)
-rootTodo.child.child.createChild('4th layer', rootTodo.child.child);
-//this will allow traversal whilst child !== null
-
-
-rootTodo.createSibling('sibling todo on top layer');
-rootTodo.child.createSibling('sibling todo on 2nd layer');
-rootTodo.child.child.createSibling('sibling todo on 3rd layer');
-
-console.log(rootTodo);
+console.log($todoList);
